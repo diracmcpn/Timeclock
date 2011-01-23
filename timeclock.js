bef23ec16319df0cfe54ae94577c3920c2bdeb87
dirac <diracmcpn@hotmail.com>
2011-01-23 04:27:25 -0800

    comment the code

--- timeclock.js
+++ timeclock.js
@@ -1,7 +1,10 @@
+//convert the number from 1 to 9 in the format 01, 02, 03, ...09 
 function add0(x)
 { 
     return (x>9)?x:"0"+x; 
 }
+
+//Print time every 1s in format HH:MM:SS in the html element "clock"
 function timeclock()
 {
      var temps = new Date();
